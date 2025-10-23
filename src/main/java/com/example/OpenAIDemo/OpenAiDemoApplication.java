ea095b2d254378b4cb3a3e76b04e08ace897df14
Tony Shangkuan <tonyshangkuan@gmail.com>
2025-10-23 12:01:57 +0800

    git init

---
+++ src/main/java/com/example/OpenAIDemo/OpenAiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenAIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiDemoApplication.class, args);
+	}
+
+}
